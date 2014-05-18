d312cb72c21bcd5a20ad298c37a90ebdbb222f29
Lars <K3LARRA@HOTMAIL.COM>
2014-05-18 23:06:03 +0200

    Cleared crap

--- SwingProj/src/LarsClass.java
+++ SwingProj/src/LarsClass.java
@@ -2,15 +2,7 @@
 public class LarsClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String s1 = "Lars";
-		int i = 23;
-		int i2 = 24;
-		int i3 = 25;
-		String testing = "But this is crap";
-		int i4 = 26; //new branch
-		int i5 = 27;
-		int i6 = 27;
+
 	}
 
 }
