3cdd99b70baf2680bb19f1ff23ddcd7a5226751f
Cogwings <fuxstag@gmail.com>
2014-05-19 10:40:18 +0200

    You are NOT prepared!!!

--- SwingProj/src/GustafClass.java
+++ SwingProj/src/GustafClass.java
@@ -3,7 +3,30 @@ public class GustafClass {
 
 	
 	public static void main(String[]args){
-		
+		/*Tyyyype the greatest code in the world... or-i'll-eat-your-soul.
+		 * 
+		 * And we typed the first thing that came to our heads, just so happend to be,
+		 *The best code in the world. 
+		 *It was the best code in the world!
+		 *Look into eclipse and it's easy to see that 
+		 * 
+		 * 
+		 * be you hackers?
+		 * and we said NAY! We are but MEN! TYPE!
+		 * AAAAAHHAAAAAUAAAA!!!!!!!!!!!!!!!!!
+		 * 
+		 * 
+		 * This is not, the greatest code in the world!..
+		 * 
+		 * This is just a tribute.
+		 * 'Couldn't remember the greatest code in the wohoorld, NO!
+		 * this is a trib-ute.(uhuwoawoa)
+		 * 
+		 * To the greatest code in the world(uhuwoa oawo)
+		 * to the greatest code in the world
+
+		 * 
+		 */
 		
 		
 	}
