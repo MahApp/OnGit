a4ca2ebe4c97ce00539a0f3e9c5456380e0efedd
Lars <K3LARRA@HOTMAIL.COM>
2014-09-30 14:01:28 +0200

    And WE ARE CHANGING again

--- Proj1/src/com/example/proj1/MainActivity.java
+++ Proj1/src/com/example/proj1/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	//more strange changes
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
