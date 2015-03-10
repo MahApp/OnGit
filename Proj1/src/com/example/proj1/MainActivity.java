4d1a24d68e95e08816abc4d461fc131e38824a06
Lars <K3LARRA@HOTMAIL.COM>
2015-03-10 16:33:44 +0100

    Shit

--- Proj1/src/com/example/proj1/MainActivity.java
+++ Proj1/src/com/example/proj1/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    	//more strange changes
+    	//morre strange changes
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
